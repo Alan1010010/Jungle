eb56f4de4eaa794f9fa08aa57e0871a0c44ebb03
Alan Quinn <alancquinn2@gmail.com>
2019-09-19 10:09:00 +0100

    make user index available to homepage, add cards for top 5 users to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+     @users = policy_scope(User).order(seeds: :desc).where.not(id: current_user.id)
   end
+
+  def home_user_index
+
+  end
+
 end
