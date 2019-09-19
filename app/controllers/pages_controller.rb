9c2b27c7ebd62d757c331f16bcae7a7dc8639af2
Alan Quinn <alancquinn2@gmail.com>
2019-09-19 11:28:03 +0100

    remove current_user from pages controller to fix  hompage error for unauth user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-     @users = policy_scope(User).order(seeds: :desc).where.not(id: current_user.id)
+     @users = policy_scope(User).order(seeds: :desc)
   end
 
   def home_user_index
