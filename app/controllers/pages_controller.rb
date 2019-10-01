d71642713b2eb4ce8b219de724d0663a2354942f
Alan Quinn <alancquinn2@gmail.com>
2019-10-01 14:15:00 +0100

    add style to paddle checkout page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :ebook]
 
   def home
      @users = policy_scope(User).order(seeds: :desc)
